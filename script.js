60e4818ae16e1852110ee67a42564b6da64e187a
nellyhsu123 <33136548+nellyhsu123@users.noreply.github.com>
2019-04-11 20:10:21 -0700

    learning about objects and functions in JS

---
+++ script.js
@@ -0,0 +1,30 @@
+// Function constructor, very similar to how constructors are made in Java
+
+// normal initialization of an object
+var john = {
+	name: 'John',
+	yearOfBirth: 1990,
+	job: 'teacher'
+}
+
+var Person = function(name, yearOfBirth, job) {
+	this.name = name;
+	this.yearOfBirth = yearOfBirth;
+	this.job = job;
+}
+
+//Prototype property which is inheritance in javscript
+Person.prototype.calculateAge = function () {
+		console.log(2016 - this.yearOfBirth);
+}
+
+Person.prototype.lastName = 'Smith';
+
+var john = new Person('John', 1990, 'teacher');
+var jane = new Person('Jane', 1969, 'designer');
+
+john.calculateAge();
+jane.calculateAge();
+
+console.log(john.lastName);
+
