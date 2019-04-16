e03d1f80207a8cb7b8d1ad73bbd807c7a8027ec3
nellyhsu123 <33136548+nellyhsu123@users.noreply.github.com>
2019-04-15 20:22:47 -0700

    couple more lessons

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 // Function constructor, very similar to how constructors are made in Java
 
 // normal initialization of an object
-var john = {
+/*var john = {
 	name: 'John',
 	yearOfBirth: 1990,
 	job: 'teacher'
@@ -27,6 +27,42 @@ john.calculateAge();
 jane.calculateAge();
 
 console.log(john.lastName);
-
+*/
 // console.info(some object)
 // will give you methods of and properties (prototype chain)
+
+// Object.create
+
+var personProto = {
+	calculateAge: function() {
+		console.log(2016 - this.yearOfBirth);
+	}
+};
+
+var john = Object.create(personProto);
+john.name = 'John';
+john.yearOfBirth = 1990;
+john.job = 'teacher';
+
+var jane = Object.create(personProto, 
+{
+	name: {value: 'Jane'},
+	yearOfBirth: { value: 1969 },
+	job: { value: 'designer' }
+});
+
+/********************************
+* Passing functions as arguments
+*/
+
+var years = [1990, 1965, 1937, 2005, 1998];
+
+funtion arrayCalc(arr, fn) {
+	var arrRes = [];
+
+	for(var i = 0; i < arr.length; ++i) {
+		
+	}
+}
+
+
