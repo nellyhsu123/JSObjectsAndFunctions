7bc27548c622d04a60c08a4182fba389ee5eaf22
nellyhsu123 <33136548+nellyhsu123@users.noreply.github.com>
2019-04-17 20:38:07 -0700

    challenge 7 part 2 complete, go over again tomorrow

--- script.js
+++ script.js
@@ -47,50 +47,36 @@ var jane = Object.create(personProto,
 
 /********************************
 * Passing functions as arguments
-
-
 var years = [1990, 1965, 1937, 2005, 1998];
-
 function arrayCalc(arr, fn) {
 	var arrRes = [];
-
 	for(var i = 0; i < arr.length; ++i) {
 		arrRes.push(fn(arr[i]));	
 	}
-
 	return arrRes;
 }
-
 function calculateAge(el) {
 	return 2016 - el;
 }
-
 function isFullAge(el) {
 	return el >= 18;
 }
-
 function maxHeartRate(el) {
 	if(el >= 18 && el <= 81) {
 		return Math.round(206.9 - (0.67 * el));
 	} else {
 		 return -1;
 	}
-
 }
-
 var ages = arrayCalc(years, calculateAge);
 var fullAges = arrayCalc(ages, isFullAge);
 var rates = arrayCalc(ages, maxHeartRate);
-
-
 console.log(ages);
 console.log(rates);
 */
 
 /***************************
 * Functions returning functions
-
-
 function interviewQuestion(job) {
 	if (job === 'designer') {
 		return function(name) {
@@ -106,11 +92,8 @@ function interviewQuestion(job) {
 			}
 		}
 	}
-
 var teacherQuestion = interviewQuestion('teacher');
-
 teacherQuestion('John');
-
 interviewQuestion('teacher')('Mark');
 */
 
@@ -118,19 +101,15 @@ interviewQuestion('teacher')('Mark');
  * Lecture: IIFE
  create a new scope to safely put variable in
  
-
  function game() {
  	var score = Math.random() * 10;
  	console.log(score >= 5);
  }
-
  game();
-
 (function () {
 	var score = Math.random() * 10;
  	console.log(score >= 5);
  })();
-
 (function (goodLuck) {
 	var score = Math.random() * 10;
  	console.log(score >= 5 - goodLuck);
@@ -140,7 +119,6 @@ interviewQuestion('teacher')('Mark');
 /*****************
  *Closures
  
-
 function retirement(retirementAge) {
 	var a = ' years left until retirement.';
 	return function(yearOfBirth) {
@@ -148,18 +126,13 @@ function retirement(retirementAge) {
 		console.log((retirementAge - age) + a);
 	}
 }
-
 var retirementUS = retirement(66);
 var retirementGermany = retirement(65);
 var retirementIceland = retirement(67);
-
-
 retirementGermany(1994);
 retirementIceland(1994);
 retirementUS(1990);
-
 //retirement(66)(1990);
-
 function interviewQuestion(job) {
 	return function(name) {
 		if (job === 'designer') {
@@ -172,14 +145,12 @@ function interviewQuestion(job) {
 		}
 	}
 }
-
 interviewQuestion('teacher')('John');
 */
 
 /************************
  * Bind, call and apply
  
-
  var john = {
  	name: 'John',
   age: 26,
@@ -192,76 +163,56 @@ interviewQuestion('teacher')('John');
  		}
  	}
  }
-
  var emily = {
  	name: 'Emily',
  	age: 35,
  	job: 'designer'
  };
-
  john.presentation('formal', 'morning');
-
 //call method allows us to set the "this" in the first argument
  john.presentation.call(emily, 'friendly', 'afternoon');
 // apply uses array as an argument
 //john.presentation.apply(emily, ['friendly', 'afternoon']);
-
 //bind returns a copy of the function
 // presets arguments
 var johnFriendly = john.presentation.bind(john, 'friendly');
-
 johnFriendly('morning');
 johnFriendly('night');
-
-
-
-
 var years = [1990, 1965, 1937, 2005, 1998];
-
 function arrayCalc(arr, fn) {
 	var arrRes = [];
-
 	for(var i = 0; i < arr.length; ++i) {
 		arrRes.push(fn(arr[i]));	
 	}
-
 	return arrRes;
 }
-
 function calculateAge(el) {
 	return 2016 - el;
 }
-
 function isFullAge(limit, el) {
 	return el >= limit;
 }
-
 var ages = arrayCalc(years, calculateAge);
 var fullJapan = arrayCalc(ages, isFullAge.bind(this, 20));
-
 console.log(ages);
 console.log(fullJapan);
-
 */
 
 /**********************
  *Coding Challenge 7 part 1
  
-
 (function() {
 	var Question = function(question, answers, cAnswer) {
 	this.question = question;
 	this.answers = answers;
 	this.cAnswer = cAnswer;
 	}
-
 	Question.prototype.display = function() {
 		console.log(this.question);
 		for(var i = 0; i < this.answers.length; ++i) {
 			console.log(this.answers[i]);
 		}
 	}
-
 	Question.prototype.checkAnswer = function(ans) {
 		if(ans === this.cAnswer) {
 			console.log('Correct answer!');
@@ -269,18 +220,12 @@ console.log(fullJapan);
 			console.log('Wrong answer :(');
 		}
 	}
-
 	var q1 = new Question('Who were the 2017 NBA Champs?', ['GSW', 'CLE', 'BOS'], 0);
 	var q2 = new Question('Who won the superbowl in 1995?', ['DAL', 'SF', 'NE'], 1);
-
 	var questions = [q1, q2];
-
 	var questionSelector = Math.floor(Math.random() * questions.length);
-
 	questions[questionSelector].display();
-
 	var answer = parseInt(prompt('Please select the correct answer.'));
-
 	questions[questionSelector].checkAnswer(answer);
 })();
 */
@@ -303,18 +248,41 @@ console.log(fullJapan);
 		}
 	}
 
-	Question.prototype.checkAnswer = function(ans) {
+	Question.prototype.checkAnswer = function(ans, callBack) {
+		var sc;
 		if(ans === this.cAnswer) {
+			
 			console.log('Correct answer!');
+			sc = callBack(true);
 		} else {
 			console.log('Wrong answer :(');
+			sc = callBack(false);
 		}
+
+		this.displayScore(sc);
+	}
+
+	Question.prototype.displayScore = function(score) {
+		console.log('Your current score is: ' + score);
+		console.log('---------------------------------');
 	}
 
 	var q1 = new Question('Who were the 2017 NBA Champs?', ['GSW', 'CLE', 'BOS'], 0);
 	var q2 = new Question('Who won the superbowl in 1995?', ['DAL', 'SF', 'NE'], 1);
 
 	var questions = [q1, q2];
+
+	function score() {
+		var sc = 0;
+		return function(correct) {
+			if(correct) {
+				sc++;
+			}
+			return sc;
+		}
+	}
+
+	var keepScore = score();
 
 	function nextQuestion() {
 	
@@ -325,7 +293,7 @@ console.log(fullJapan);
 		var answer = prompt('Please select the correct answer.');
 
 		if(answer !== 'exit') {
-			questions[questionSelector].checkAnswer(parseInt(answer));
+			questions[questionSelector].checkAnswer(parseInt(answer), keepScore);
 			nextQuestion();
 		}
 		
@@ -334,7 +302,3 @@ console.log(fullJapan);
 	nextQuestion();
 	
 })();
-
-
-
-
